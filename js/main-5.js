bf1f04bb62a0e57e3d124179504d049998c41077
lizyweb <147726740+lizyweb@users.noreply.github.com>
2025-03-20 17:44:18 +0530

    Add files via upload

---
+++ js/main-5.js
@@ -0,0 +1,65 @@
+document.addEventListener('DOMContentLoaded', () => {
+    "use strict";
+  
+       // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 45) {
+            $('.navbar').addClass('sticky-top shadow-sm');
+        } else {
+            $('.navbar').removeClass('sticky-top shadow-sm');
+        }
+    });
+
+
+    // Hero Header carousel
+    $(".header-carousel").owlCarousel({
+        animateOut: 'fadeOut',
+        items: 1,
+        margin: 0,
+        stagePadding: 0,
+        autoplay: true,
+        smartSpeed: 500,
+        dots: true,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-arrow-left"></i>',
+            '<i class="bi bi-arrow-right"></i>'
+        ],
+    });
+
+
+    // testimonial carousel
+   $(".testimonial-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1500,
+    center: false,
+    dots: true,
+    loop: true,
+    margin: 25,
+    nav : true,
+    navText : [
+        '<i class="fa fa-angle-right"></i>',
+        '<i class="fa fa-angle-left"></i>'
+    ],
+    responsiveClass: true,
+    responsive: {
+        0:{
+            items:1
+        },
+        576:{
+            items:1
+        },
+        768:{
+            items:1
+        },
+        992:{
+            items:2
+        },
+        1200:{
+            items:2
+        }
+    }
+});
+
+});
